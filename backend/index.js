4ef666c394042e37e89bb4bbac97c7ed85af5c51
mighty808 <effahaboagye888@gmail.com>
2026-01-15 00:42:08 +0000

    moved db from localhost to mongo Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectDb from "./db/connectDB.js";
-import { insertOne } from "./models/Transaction.js";
+import { allDocs } from "./models/Transaction.js";
 
 const app = express();
 
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8000;
 const MONGODB_URI = process.env.MONGODB_URI;
 connectDb(MONGODB_URI);
 // console.clear();
-insertOne();
+allDocs();
 
 app.listen(port, () => {
   console.log(`Server is running on Port ${port}`);
--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -14,23 +14,21 @@ const transactionSchema = new mongoose.Schema({
 const TransactionModel = mongoose.model("Transaction", transactionSchema);
 
 // Getting all
-/*
   const allDocs = async () => {
   const results = await TransactionModel.find();
   console.log(results);
 };
-*/
 
-const insertOne = async () => {
-  const doc = new TransactionModel({
-    transactionId: "53479513587",
-    recipientName: "Mark Atta",
-    recipientPhoneNumber: "0206874512",
-    amount: 2200,
-  });
+// const insertOne = async () => {
+//   const doc = new TransactionModel({
+//     transactionId: "53479513587",
+//     recipientName: "Mark Atta",
+//     recipientPhoneNumber: "0206874512",
+//     amount: 2200,
+//   });
 
-  const result = await doc.save();
-  console.log("Inserted:", result);
-};
+//   const result = await doc.save();
+//   console.log("Inserted:", result);
+// };
 
-export { insertOne };
+export { allDocs };
