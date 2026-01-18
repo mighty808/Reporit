4e438562861c727472c083bf41498f476568782f
mighty808 <effahaboagye888@gmail.com>
2026-01-18 23:49:26 +0000

    Final touches on Alerts page, done but will enhance

--- frontend/src/pages/AlertsPage.jsx
+++ frontend/src/pages/AlertsPage.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
-import CheckForm from '../components/CheckForm'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import AlertsFeed from "../components/AlertsFeed";
 
 const AlertsPage = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const handleMenuToggle = (isOpen) => {
-    setIsMobileMenuOpen(isOpen)
-  }
+    setIsMobileMenuOpen(isOpen);
+  };
 
   return (
     <>
       <Navbar onMenuToggle={handleMenuToggle} />
       <Sidebar isMobileMenuOpen={isMobileMenuOpen}>
-        
+        <AlertsFeed />
       </Sidebar>
     </>
-  )
-}
+  );
+};
 
-export default AlertsPage
+export default AlertsPage;
